2031dff65a9f746f1c4b86ff6f9f18a672b82699
Gail Mitchell <g36mitchell@gamil.com>
2019-09-21 23:24:59 -0500

    on submit event

--- index.js
+++ index.js
@@ -1,36 +1,102 @@
 "use strict"
 
-const questionPerQuiz = 5;
-const quizMaterial = baseball;
-let questionList = [];
-let thisQuiz = { currentQuestion: 0,
-                 correctQuestions: 0,
-                 incorrectQuestions: 0};
+const questionPerQuiz = 5;   /* variable for the length of quiz */
+const quizMaterial = baseball;  /* variable for the content of the quiz in quiz.js*/
+let questionList = [];   /* randomized list of indexes to access the quiz content */
 
+function startNewQuiz(thisQuiz) {
+
+	console.log("startNewQuiz");
+	$('.js-start-button').click( function() {
+		console.log("insideStartNewQuiz");
+		initializeQuiz(thisQuiz);
+		renderQuestion(quizMaterial[questionList[thisQuiz.current]]);
+	});
+}
+
+function initializeQuiz(thisQuiz) {
+	thisQuiz.current = 0;
+	thisQuiz.right = 0;
+	thisQuiz.wrong = 0;
+	questionList = [];
+
+	$('.js-start-quiz').hide();   /* hide the start button */
+	$('.js-ask-question').show(); /* show the question and answer section now */
+	updateScoreBoard(thisQuiz, 
+					 questionPerQuiz);  /* display the scoreboard */
+
+    /* randomize the quiz material to keep the quiz interesting */
+	createListOfQuestions(questionList, quizMaterial);
+	shuffleList(questionList);
+}
+
+
+function updateScoreBoard(thisQuiz, total) {
+
+	$('#js-currentQuestion').html(thisQuiz.current + 1);  /*currentQuestion is an arry pointer */
+	$('#js-totalQuestions').html(total);
+	$('#js-correct').html(thisQuiz.right);
+	$('#js-incorrect').html(thisQuiz.wrong);
+	$('#js-score').html((thisQuiz.right/(++total) * 100).toFixed(1));
+	$('.js-display-results').show();
+}
+
+/*-- RANDOMIZE question from question library to make quiz interesting --*/
+function createListOfQuestions(q, m) {
+	for (let i = 0; i < m.length; i++) { 
+		 q.push(i); 
+	}
+}
+function shuffleList(arr) {
+	/* Fisher-Yates shuffle algorithm */
+	for (let i = arr.length - 1; i > 0 ;  i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		let t = arr[i];
+		arr[i] = arr[j];
+		arr[j] = t;
+	}
+}
+/* --------------------------------------------------------------------- */
+
+
+function renderQuestion (questionObject) {
+
+	/* send the object from the quizMaterial to generate the HTML */
+	const questionAnswerString = generateQuestionAnswerString(questionObject);
+
+	/* render the HTML */
+	$('.js-display-question').html(questionAnswerString);
+	$('.js-display-question').show();
+	$('.js-display-response').hide();
+}
 
 function generateQuestionAnswerString(q) {
- 
+
+	/*  randomize the radio button content to make the quiz more interesting */
 	let randomAnswers = [0,1,2,3];
 	shuffleList(randomAnswers);
 
 	return 	`
-  <legend aria-live="assertive" >${q.question}</legend>
-  	
-	<input  id="option1" type="radio" name="options"  value="${q.answers[randomAnswers[0]].answer}" data-item-id="${q.answers[randomAnswers[0]].aid}">
-	<label for="option1">${q.answers[randomAnswers[0]].answer}</label>
-	<br/>
-	<input  id="option2" type="radio" name="options"  value="${q.answers[randomAnswers[1]].answer}" data-item-id="${q.answers[randomAnswers[1]].aid}">
-	<label for="option2">${q.answers[randomAnswers[1]].answer}</label>
-	<br/>
-	<input  id="option3" type="radio" name="options" value="${q.answers[randomAnswers[2]].answer}" data-item-id="${q.answers[randomAnswers[2]].aid}">
-	<label for="option3">${q.answers[randomAnswers[2]].answer}</label>
-	<br/>
-	<input  id="option4" type="radio" name="options" value="${q.answers[randomAnswers[3]].answer}" data-item-id="${q.answers[randomAnswers[3]].aid}">
-	<label for="option4">${q.answers[randomAnswers[3]].answer}</label>
-  
-  <div class="js-submit-answer">
-	 <button class="input-button js-input-button" type="button" disabled>Submit Answer</button>
-  </div>
+ 	<fieldset>
+	 	<div>
+			  <legend aria-live="assertive" >${q.question}</legend>
+	 	</div>
+		<div>
+		 	<input  checked id="option1" type="radio" name="options"  value="${q.answers[randomAnswers[0]].answer}" data-item-id="${q.answers[randomAnswers[0]].aid}">
+			<label         for="option1">${q.answers[randomAnswers[0]].answer}</label>
+			<br/>
+			<input  id="option2" type="radio" name="options"  value="${q.answers[randomAnswers[1]].answer}" data-item-id="${q.answers[randomAnswers[1]].aid}">
+			<label for="option2">${q.answers[randomAnswers[1]].answer}</label>
+			<br/>
+			<input  id="option3" type="radio" name="options" value="${q.answers[randomAnswers[2]].answer}" data-item-id="${q.answers[randomAnswers[2]].aid}">
+			<label for="option3">${q.answers[randomAnswers[2]].answer}</label>
+			<br/>
+			<input  id="option4" type="radio" name="options" value="${q.answers[randomAnswers[3]].answer}" data-item-id="${q.answers[randomAnswers[3]].aid}">
+			<label for="option4">${q.answers[randomAnswers[3]].answer}</label></div>
+  		<div class="js-submit-answer">
+	 		<button class="input-button js-input-button" type="submit">Submit Answer</button>
+		</div>
+	</fieldset>
    `;
 }
 
@@ -49,18 +115,7 @@ function generateResponseAnswerString(q, truth) {
 
 }
 
-function renderQuestion (questionObject) {
 
-	   /* send the object from the quizMaterial to generate the HTML */
-	   const questionAnswerString = generateQuestionAnswerString(questionObject);
-
-	   $('.js-ask-question').show();
-	   $('.js-display-question').html(questionAnswerString);
-	   $('.js-display-question input').attr('disabled', false);
-	   $('.js-display-question').show();
-	   $('.js-display-response').hide();
-
-}
 
 function renderResponse (questionObject, truth) {
 
@@ -72,19 +127,11 @@ function renderResponse (questionObject, truth) {
 		$('.js-display-response').show();
 }
 
-function shuffleList(arr) {
-		/* Fisher-Yates shuffle algorithm */
-		for (let i = arr.length - 1; i > 0 ;  i--) {
-	        let j = Math.floor(Math.random() * (i + 1));
-			let t = arr[i];
-			arr[i] = arr[j];
-			arr[j] = t;
-		}
-}
 
-function completeQuiz() {
 
-	let score = (thisQuiz.correctQuestions / (thisQuiz.correctQuestions + thisQuiz.incorrectQuestions)) * 100;
+function completeQuiz(thisQuiz) {
+
+	let score = (thisQuiz.right / (thisQuiz.right + thisQuiz.wrong)) * 100;
 	let finalText =  (score > 79 ) ?
 		`Your score is ${score}%.  You know your baseball!  Start the quiz again to see more questions to test your knowledge.` :
 		`Your score is ${score}%.  You have learned some new things about baseball. Start the quiz again to learn even more!`;
@@ -94,111 +141,79 @@ function completeQuiz() {
 	 $('.js-ask-question').hide();
 }
 
-function initializeForNewQuiz() {
-		thisQuiz.currentQuestion = 0;
-		thisQuiz.correctQuestions = 0;
-		thisQuiz.incorrectQuestions = 0;
-
-		$('.js-start-quiz').hide();
-		displayTotals(0,0,0);
-		$('.js-display-question').show();
-		$('.js-display-results').show();
-		$('#js-currentQuestion').html(thisQuiz.currentQuestion + 1);
-		$('#js-totalQuestions').html(questionPerQuiz);
-}
 
 
-function createListOfQuestions(q, m) {
-		
-	    for (let i = 0; i < m.length; i++) { 
-		     q.push(i); 
-        }
-}
-
-
-function nextQuestion() {
+function nextQuestion(thisQuiz) {
 
 	$('.js-display-response').on('click', 'button', function(event) {
 
-		 ++thisQuiz.currentQuestion;
+		 ++thisQuiz.current;  /* increment the question */
 		 $('.js-display-response').hide();
 		
-		  if ( thisQuiz.currentQuestion < questionPerQuiz ) {
-
-			    $('#js-currentQuestion').html(thisQuiz.currentQuestion + 1);
-				renderQuestion(quizMaterial[questionList[thisQuiz.currentQuestion]]);
+		  if ( thisQuiz.current < questionPerQuiz ) {
+			    updateScoreBoard(thisQuiz, 
+			    				 questionPerQuiz);  /* display the scoreboard */
+				renderQuestion(quizMaterial[questionList[thisQuiz.current]]);
 		  }
 		  else {
-			    completeQuiz();
+			    completeQuiz(thisQuiz);
 		  }
 
 	});
 }
 
 
-function checkSubmittedAnswer() {
+function checkSubmittedAnswer(thisQuiz) {
 
-		$('.js-display-question').on('click', 'button', function(event) {
-					
+		console.log($('.js-display-question'));
+
+		
+		$('#js-question').on('submit', function(event) {
+			
+			console.log("Inside the submit button action");
+
+			event.preventDefault();  // ignore default responses for form 
+
+
 			let answerID = $('.js-display-question input:checked').attr('data-item-id');
 			let foundAnswer = false;
 			let selectedIsTrue = false;
 
-			for (let x = 0; x < quizMaterial[questionList[thisQuiz.currentQuestion]].answers.length && !foundAnswer; x++) {
+			
+			for (let x = 0; x < quizMaterial[questionList[thisQuiz.current]].answers.length && !foundAnswer; x++) {
 
-				if (quizMaterial[questionList[thisQuiz.currentQuestion]].answers[x].aid === answerID) {
+				if (quizMaterial[questionList[thisQuiz.current]].answers[x].aid === answerID) {
 					foundAnswer = true;
-					selectedIsTrue = quizMaterial[questionList[thisQuiz.currentQuestion]].answers[x].correct;
+					selectedIsTrue = quizMaterial[questionList[thisQuiz.current]].answers[x].correct;
 
-					(selectedIsTrue) ? thisQuiz.correctQuestions++ : thisQuiz.incorrectQuestions++;
+					(selectedIsTrue) ? thisQuiz.right++ : thisQuiz.wrong++;
 				}
 			}
 
-			renderResponse(quizMaterial[questionList[thisQuiz.currentQuestion]],selectedIsTrue); 
-			displayTotals(thisQuiz.currentQuestion, thisQuiz.correctQuestions, thisQuiz.incorrectQuestions);
+			renderResponse(quizMaterial[questionList[thisQuiz.current]],selectedIsTrue); 
+			updateScoreBoard(thisQuiz, 
+							 questionPerQuiz);  /* display the scoreboard */
 
 		});
 }
 
-function displayTotals(total, right, wrong) {
-
-	 $('#js-correct').html(right);
-	 $('#js-incorrect').html(wrong);
-	 $('#js-score').html((right/(++total) * 100).toFixed(1));
-}
-
-function enableSubmittingAnswer() {
-
-		$('.js-display-question').on('click', 'input[type=radio]', function(event) {
-			$('.js-display-question button').attr('disabled', false);
-		});
-}
 
 function disableSubmittingAnotherAnswer() {
 		$('.js-display-question button').attr('disabled', true);
 		$('.js-display-question input').attr('disabled', true);
 }
 
-function startNewQuiz() {
-
-	$('.js-display-results').hide();
-	$('.js-ask-question').hide();
-
-		$('.js-start-quiz').click( function() {
-			initializeForNewQuiz();
-			createListOfQuestions(questionList, quizMaterial);
-			shuffleList(questionList);
-			renderQuestion(quizMaterial[questionList[0]]);
-			$('#js-currentQuestion').html(thisQuiz.currentQuestion + 1);
-			$('#js-totalQuestions').html(questionPerQuiz);
-		});
-}
 
 function handleQuiz() {
-			startNewQuiz();
-	  		enableSubmittingAnswer();
-			checkSubmittedAnswer();
-			nextQuestion();  
+
+	let thisQuiz = { current: 0,
+					 right: 0,
+					 wrong: 0
+					};
+
+	startNewQuiz(thisQuiz);
+	checkSubmittedAnswer(thisQuiz);
+	nextQuestion(thisQuiz);  
 }
 
 $(handleQuiz);
